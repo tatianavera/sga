327f018810ed3b122ca5b1924ba3c4b45d3e842f
tatianavera <55669162+tatianavera@users.noreply.github.com>
2019-09-22 16:23:50 -0500

    update docente estilos css

--- view/docente/asistencias.php
+++ view/docente/asistencias.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['USER_UEAS'])) {
       <meta charset="utf-8">
       <title>Mis Cursos</title>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <link type="text/css" rel="stylesheet" href="../../css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
@@ -104,7 +104,7 @@ if (isset($_SESSION['USER_UEAS'])) {
       </footer>
 
         <script type="text/javascript" src="../../js/jquery-3.4.1.js"></script>
-      <script type="text/javascript" src="../../js/materialize.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script type="text/javascript" src="../../js/index.js"></script>
 
         <style media="screen">
--- view/docente/index.php
+++ view/docente/index.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['USER_UEAS'])) {
   </meta>
   <title>Home</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <link type="text/css" rel="stylesheet" href="../../css/administrador_index.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
